50d2d0e590c5bbdc904026615f7f0f3b3303a1bb
Jeffrey Takyi-Yeboah <jeffyeb90@gmail.com>
2019-11-25 21:18:31 -0600

    Kafka_files

---
+++ project/generate-files.py
@@ -0,0 +1,23 @@
+#Python 3.7.2
+#!/usr/bin/python
+import re
+import os
+import collections
+import time
+
+def buildIndex():
+	
+        currentPath = "/Users/jeffreytakyi-yeboah/Desktop/project/test.MP4"
+                
+        currentdir = sorted(os.listdir(currentPath))
+               
+        for currentfile in currentdir:
+                if currentfile.endswith(".txt"):
+                        
+                        my_file = os.path.join(currentPath, currentfile)
+                #print(my_file)
+                        print(currentfile)
+                                    
+
+               
+        return
